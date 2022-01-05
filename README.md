4c72d345ddcb30c0e9e9001c20e0cc0b24d6845a
DPS2004 <dpsophinos@gmail.com>
2022-01-05 15:53:42 -0500

    step 1

--- README.md
+++ README.md
@@ -1,9 +1,33 @@
-# leshycam
+# Leshy's Camera
 Generate Inscryption styled portrait sprites from any image.
 
+# Setup and Config
+This project uses PIL and ConfigParser.
+
+The following values can be edited in config.ini:
+
+images_in: The folder that Leshy's Camera uses as input
+
+images_out: The folder that Leshy's Camera uses as input
+
+palette: The number of colors per card. Set to 0 for a random palette each image.
+
+
 # Notes on colors:
-Most cards use 3 brightness levels, 000, 127-150,and 255.
+Most cards use either 2 brightness levels, 000 and 255
+
+or 3 brightness levels, 000, 140, and 255
+
 I'd guess that 90% of the cards use this.
+
 Other level counts I found were:
-4 (used for Alpha, Caged Wolf): 000, 90, 170, 255
-5 (used for Amalgam, Lice): (todo)
+
+4 (used for Alpha, Caged Wolf): 000, 100, 170, 255
+
+5 (used for Amalgam, Lice): 000, 092, 127, 175, 255
+
+Exceptions:
+
+Greater Smoke, 8 colors: 000, 051, 70, 100, 127, 143, 191, 255
+
+Undead Cat, 3 colors: 0, 51, FF0000 (red)
--- inscrybe.py
+++ inscrybe.py
@@ -1,28 +1,36 @@
 import sys
 import os
 from PIL import Image
+import random
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+conf = config['main']
 
 # adapted from https://stackoverflow.com/a/29438149
-def quantizetopalette(silf, palette, dither=False):
+def quantizetopalette(silf, pal, dither=False):
     """Convert an RGB or L mode image to use a given P image's palette."""
 
     silf.load()
 
-    palette.load()
-    if palette.mode != "P":
+    pal.load()
+    if pal.mode != "P":
         raise ValueError("bad mode for palette image")
     if silf.mode != "RGB" and silf.mode != "L":
         raise ValueError(
             "only RGB or L mode images can be quantized to a palette"
             )
-    im = silf.im.convert("P", 1 if dither else 0, palette.im)
-
-    try:
-        return silf._new(im)
-    except AttributeError:
-        return silf._makeself(im)
+    silf = silf.quantize(colors=256,palette=pal,dither=Image.NONE)
+    return silf
 def makepaletteimage(palettedata):
-
+    for x in range(0,len(palettedata)):
+        col = palettedata[x*3]
+        for i in range(0,2):
+            palettedata.insert(x*3+i,col)
+            
+    
     NUM_ENTRIES_IN_PILLOW_PALETTE = 256
     num_bands = len("RGB")
     num_entries_in_palettedata = len(palettedata) // num_bands
@@ -34,28 +42,46 @@ def makepaletteimage(palettedata):
     palimage = Image.new('P', arbitrary_size)
     palimage.putpalette(palettedata)
     return palimage
+    
 
-basepath = "cards/"
+inpath = conf['images_in']
+outpath = conf['images_out']
+cpal = int(conf['palette'])
 
-# NOTES ON COLORS:
-# Most cards use 3 colors, 000,127,and 255.
-# I'd guess that 90% of the cards use this.
-# The other most common color count i found was five, used on cards like Amalgam.
-# inscryption uses 5 colors, including black and white.
+palettes = {
+    2: [0,255],
+    3: [0,140,255],
+    4: [0,100,170,255],
+    5: [0,97,127,175,255],
+    8: [0,51,70,100,127,143,191,255]
+}
 
-makepaletteimage([0,0,0,
+randpal = False
 
-print("loading images...")
+if cpal == 0:
+    randpal = True
+    cpal = 2
+    
+palimg = makepaletteimage(palettes[cpal])
+
+try: 
+    os.mkdir(outpath) 
+    print("loading images...")
+except:
+    print("loading images...")
+    # wow cool coding practices nice job
 images = []
-for imgfn in os.listdir(basepath):
-    print(imgfn)
-
-    ogimg = Image.open(basepath + imgfn)
+for imgfn in os.listdir(inpath):
+    print('Photographing ' + imgfn)
+    if randpal:
+        cpal = random.randint(2,5)
+        palimg = makepaletteimage(palettes[cpal])
     
+    # Step 1: quantize to the palette
+    ogimg = Image.open(inpath + imgfn)
+    newimg = quantizetopalette(ogimg,palimg)
     
-
-    ogimg.save("out.png")    
-    break
+    newimg.save(outpath + imgfn)
 print("done!")
 
 
